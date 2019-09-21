fa0aa6837f3d2544490a184e359834750c8be946
Fernando Ruiz <linuxuser07@gmail.com>
2019-09-20 19:17:00 -0500

    Learning React JSX format and component Usage

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react app </h1>
+        <p>This is working </p>
+        <Person/>
       </div>
     );
+    //this is what the jsx is compile to 
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'this does work'));
   }
 }
 
