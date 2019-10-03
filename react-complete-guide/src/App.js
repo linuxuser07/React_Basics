a846be396457b96a2ad24fbd0b9c8d2ab9b8e695
Fernando Ruiz <linuxuser07@gmail.com>
2019-10-02 21:24:03 -0500

    Finished initial state and stateless changes also added some styling and was able to do global and inline styling.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Person from './Person/Person';
 
 const App = props => {
+  
+  //container of state try to limit as much as possible. 
+  //smart stateful component 
   const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Max', age: 28 },
@@ -11,27 +14,47 @@ const App = props => {
     ]
   });
 
-  const [otherState, setOtherState] = useState('some other value');
+ // const [otherState, setOtherState] = useState('some other value');
 
-  console.log(personsState, otherState);
+  //console.log(personsState, otherState);
 
-  const switchNameHandler = () => {
+  const switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
     setPersonsState({
       persons: [
-        { name: 'Maximilian', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 }
       ]
     });
   };
 
+  const nameChangeHandler = (event) => {
+    setPersonsState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    });
+  };
+
+  //unable to use all the css features 
+  const style ={
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  };
+  
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is really working!</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
+      <button style = {style}
+      onClick={() => switchNameHandler('Maximilian!!!')}>Switch Name</button>
       <Person
         name={personsState.persons[0].name}
         age={personsState.persons[0].age}
@@ -39,6 +62,9 @@ const App = props => {
       <Person
         name={personsState.persons[1].name}
         age={personsState.persons[1].age}
+        //use bind syntax to get better performance
+        click={switchNameHandler.bind(this, 'Max!')}
+        changed ={nameChangeHandler}
       >
         My Hobbies: Racing
       </Person>
